27bd68fdaa52020a20771a6d0f9f446a12b3dd7e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-05 01:08:23 -0300

    feat: implement new GraphQL Mutation to add new user

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -2,6 +2,10 @@ const userResolvers = {
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
     user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
+  },
+
+  Mutation: {
+    addUser: (root, user, { dataSources }) => dataSources.usersAPI.addUser(user)
   }
 }
 
