ee24d63a51cddf8bfb4b6af1e5b8cd543ba135e5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-05 08:28:10 -0300

    feat: create method in datasource to update an user by his id

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -35,6 +35,17 @@ class UsersAPI extends RESTDataSource {
       role: role[0]
     })
   }
+
+  async updateUser(user) {
+    const role = await this.get(`/roles?type=${user.role}`)
+
+    await this.put(`/users/${user.id}`, { ...user, role: role[0].id })
+
+    return ({
+      ...user,
+      role: role[0]
+    })
+  }
 }
 
 module.exports = UsersAPI
