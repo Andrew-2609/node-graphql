63d30f9ea51e151c4a9fcb1767ebd52759b9a46f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-05 09:24:30 -0300

    feat: define new GraphQLScalarType called DateTime

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,4 +1,14 @@
+const { GraphQLScalarType } = require('graphql')
+
 const userResolvers = {
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'string with date and time in ISO-8601 format',
+    serialize: (value) => new Date(value).toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value)
+  }),
+
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
     user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
