3b36f6b962b46d2230f3355b368b168f80f1876f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-05 22:16:17 -0300

    refactor: change object positions in code

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,13 +1,6 @@
 const { GraphQLScalarType } = require('graphql')
 
 const userResolvers = {
-  // for study purposes; in this case, it isn't really necessary, since key and value are equal strings
-  RolesType: {
-    STUDENT: 'STUDENT',
-    INSTRUCTOR: 'INSTRUCTOR',
-    COORDINATION: 'COORDINATION'
-  },
-
   DateTime: new GraphQLScalarType({
     name: 'DateTime',
     description: 'string with date and time in ISO-8601 format',
@@ -15,6 +8,13 @@ const userResolvers = {
     parseValue: (value) => new Date(value),
     parseLiteral: (ast) => new Date(ast.value)
   }),
+
+  // for study purposes; in this case, it isn't really necessary, since key and value are equal strings
+  RolesType: {
+    STUDENT: 'STUDENT',
+    INSTRUCTOR: 'INSTRUCTOR',
+    COORDINATION: 'COORDINATION'
+  },
 
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
